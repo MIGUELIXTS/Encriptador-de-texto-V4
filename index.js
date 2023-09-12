f0209e32e8f4cd67a5d8d500b07883d10e7e17f4
Miguel Angel Bravo Alvarez <bravomiguel853@gmail.com>
2023-09-12 13:15:54 -0600

    Encriptador 1.0v

--- index.js
+++ index.js
@@ -5,11 +5,33 @@ function encriptar() {
     let muñeco = document.getElementById("muñeco");
   
     let textoCifrado = texto
-      .replace(/e/gi, "ʣ")
-      .replace(/i/gi, "ʦ")
-      .replace(/a/gi, "Œ")
-      .replace(/o/gi, "Æ")
-      .replace(/u/gi, "Њ");
+      .replace(/a/gi, "╔")
+      .replace(/b/gi, "╩")
+      .replace(/c/gi, "╦")
+      .replace(/d/gi, "╠")
+      .replace(/e/gi, "═")
+      .replace(/f/gi, "╬")
+      .replace(/g/gi, "╧")
+      .replace(/h/gi, "╨")
+      .replace(/i/gi, "▄")
+      .replace(/j/gi, "°")
+      .replace(/k/gi, "░")
+      .replace(/l/gi, "┤")
+      .replace(/m/gi, "♥")
+      .replace(/n/gi, "•")
+      .replace(/ñ/gi, "♦")
+      .replace(/o/gi, "∟")
+      .replace(/p/gi, "Œ")
+      .replace(/q/gi, "©")
+      .replace(/r/gi, "⌠")
+      .replace(/s/gi, "≈")
+      .replace(/t/gi, "§")
+      .replace(/u/gi, "£")
+      .replace(/v/gi, "&")
+      .replace(/w/gi, "™")
+      .replace(/x/gi, "š")
+      .replace(/y/gi, "±")
+      .replace(/z/gi, "ø");
   
     if (texto.length != 0) {
       document.getElementById("texto").value = textoCifrado;
@@ -31,11 +53,33 @@ function encriptar() {
     let muñeco = document.getElementById("muñeco");
   
     let textoCifrado = texto
-      .replace(/ʣ/gi, "e")
-      .replace(/ʦ/gi, "i")
-      .replace(/Œ/gi, "a")
-      .replace(/Æ/gi, "o")
-      .replace(/Њ/gi, "u");
+      .replace(/╔/gi, "a")
+      .replace(/╩/gi, "b")
+      .replace(/╦/gi, "c")
+      .replace(/╠/gi, "d")
+      .replace(/═/gi, "e")
+      .replace(/╬/gi, "f")
+      .replace(/╧/gi, "g")
+      .replace(/╨/gi, "h")
+      .replace(/▄/gi, "i")
+      .replace(/°/gi, "j")
+      .replace(/░/gi, "k")
+      .replace(/┤/gi, "l")
+      .replace(/♥/gi, "m")
+      .replace(/•/gi, "n")
+      .replace(/♦/gi, "ñ")
+      .replace(/∟/gi, "o")
+      .replace(/Œ/gi, "p")
+      .replace(/©/gi, "q")
+      .replace(/⌠/gi, "r")
+      .replace(/≈/gi, "s")
+      .replace(/§/gi, "t")
+      .replace(/£/gi, "u")
+      .replace(/&/gi, "v")
+      .replace(/™/gi, "w")
+      .replace(/š/gi, "x")
+      .replace(/±/gi, "y")
+      .replace(/ø/gi, "z");
     
       if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
